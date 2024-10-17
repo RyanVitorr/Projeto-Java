4f28115a543561975bab7c2435529a34774de25c
Rpclemente <rfclementez@gmail.com>
2024-10-17 20:56:01 -0300

    Adição de nomes reais aos livros e adição da interface e implementação dos livros

--- java/biblioteca/dao/LivroDAO.java
+++ java/biblioteca/dao/LivroDAO.java
@@ -1,1 +1,17 @@
 // Interface LivroDAO 
+
+/**
+ * LivroDAO
+ */
+public interface LivroDAO {
+
+    Livro salva(Livro livro);
+
+    Livro buscarPorAutor(String autor);
+
+    Livro buscarPorGenero(String genero);
+
+    Livro buscarPorTitulo(String titulo);
+
+    void remover (Livro livro);
+}
--- java/biblioteca/dao/LivroDAOImpl.java
+++ java/biblioteca/dao/LivroDAOImpl.java
@@ -1,1 +1,52 @@
 // Implementação LivroDAOImpl 
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.NoResultException;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LivroDAOImpl implements LivroDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Livro salvar(Livro livro) {
+        entityManager.persist(livro);
+        return livro;
+    }
+
+    @Override
+    public List<Livro> buscarPorAutor(String autor) {
+        String jpql = "SELECT l FROM Livro l WHERE l.autor = :autor";
+        return entityManager.createQuery(jpql, Livro.class)
+                .setParameter("autor", autor)
+                .getResultList();
+    }
+
+    @Override
+    public List<Livro> buscarPorGenero(String genero) {
+        String jpql = "SELECT l FROM Livro l WHERE l.genero = :genero";
+        return entityManager.createQuery(jpql, Livro.class)
+                .setParameter("genero", genero)
+                .getResultList();
+    }
+
+    @Override
+    public List<Livro> buscarPorTitulo(String titulo) {
+        String jpql = "SELECT l FROM Livro l WHERE l.titulo = :titulo";
+        return entityManager.createQuery(jpql, Livro.class)
+                .setParameter("titulo", titulo)
+                .getResultList();
+    }
+
+    @Override
+    public void remover(Livro livro) {
+        if (entityManager.contains(livro)) {
+            entityManager.remove(livro);
+        } else {
+            entityManager.remove(entityManager.merge(livro));
+        }
+    }
+}
